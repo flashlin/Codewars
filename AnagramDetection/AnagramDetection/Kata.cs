2d51e061a47ea5624ef71edc9bd7ff60493b2777
flash <flash.lin@gmail.com>
2018-04-12 17:27:07 +0800

    Init

---
+++ AnagramDetection/AnagramDetection/Kata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AnagramDetection
+{
+    public class Kata
+    {
+        public static bool IsAnagram(string test, string original)
+        {
+            // your code goes here
+
+            return false;
+        }
+    }
+}
---
+++ AnagramDetection/AnagramDetectionTests/Test1.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnagramDetection;
+
+namespace AnagramDetectionTests
+{
+    [TestFixture]
+    public class Test1
+    {
+        [Test]
+        [TestCase("foefet", "toffee", ExpectedResult = true)]
+        [TestCase("Buckethead", "DeathCubeK", ExpectedResult = true)]
+        [TestCase("Twoo", "Woot", ExpectedResult = true)]
+        [TestCase("apple", "pale", ExpectedResult = false)]
+        public bool FixedTest(string test, string original)
+        {
+            return Kata.IsAnagram(test, original);
+        }
+    }
+}
