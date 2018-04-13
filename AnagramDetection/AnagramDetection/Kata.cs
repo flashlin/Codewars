0712255b7f0de2238a0017d27b5e6136b6714fa7
flash <flash.lin@gmail.com>
2018-04-13 16:46:08 +0800

    add

--- AnagramDetection/AnagramDetection/Kata.cs
+++ AnagramDetection/AnagramDetection/Kata.cs
@@ -1,14 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AnagramDetection
 {
     public class Kata
     {
+        public class CharacterDic
+        {
+            private Dictionary<char, int> _dic = new Dictionary<char, int>();
+
+            public int this[char key]
+            {
+                get
+                {
+                    key = char.ToLower(key);
+                    if (!_dic.ContainsKey(key))
+                    {
+                        _dic[key] = 0;
+                    }
+                    return _dic[key];
+                }
+                set
+                {
+                    key = char.ToLower(key);
+                    _dic[key] = value;
+                }
+            }
+        }
+
         public static bool IsAnagram(string test, string original)
         {
-            // your code goes here
+            if (test.Length != original.Length)
+            {
+                return false;
+            }
+            var dic = new CharacterDic();
+            foreach (var character in original)
+            {
+                dic[character]++;
+            }
 
-            return false;
+            foreach (var character in test)
+            {
+                if (dic[character] == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
