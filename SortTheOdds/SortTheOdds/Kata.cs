e9bac4be8293e32b554631d7f223a51b998e7bd1
flash <flash.lin@gmail.com>
2018-04-16 13:00:52 +0800

    add

--- SortTheOdds/SortTheOdds/Kata.cs
+++ SortTheOdds/SortTheOdds/Kata.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SortTheOdds
 {
@@ -6,7 +7,44 @@ namespace SortTheOdds
     {
         public static int[] SortArray(int[] array)
         {
-            return array;
+            var odds = new List<int>();
+            AddOddsToList(array, odds);
+
+            odds.Sort();
+
+            int idx = 0;
+            var result = new List<int>();
+            foreach (var number in array)
+            {
+                if (IsOdds(number))
+                {
+                    result.Add(odds[idx]);
+                    idx++;
+                }
+                else
+                {
+                    result.Add(number);
+                }
+            }
+            return result.ToArray();
+        }
+
+        private static void AddOddsToList(int[] array, List<int> odds)
+        {
+            foreach (var number in array)
+            {
+                if (IsOdds(number))
+                {
+                    odds.Add(number);
+                }
+            }
+        }
+
+        public static bool IsOdds(int number)
+        {
+            if (number == 0) return false;
+            if (number % 2 == 0) return false;
+            return true;
         }
     }
 }
