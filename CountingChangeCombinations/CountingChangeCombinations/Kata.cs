552594768fda5ee765a27319295f9b1451476b69
flash <flash.lin@gmail.com>
2018-04-20 10:51:20 +0800

    add

---
+++ CountingChangeCombinations/CountingChangeCombinations/Kata.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace CountingChangeCombinations
+{
+    public class Kata
+    {
+        public static int CountCombinations(int money, int[] coins)
+        {
+            int n = FindCombination(money, coins, 0);
+            return n;
+        }
+
+        private static int FindCombination(int money, int[] coins, int checkIndex)
+        {
+            if (money == 0) return 1;
+
+            if (money < 0 || coins.Length == checkIndex)
+            {
+                return 0;
+            }
+
+            int firstCoin = FindCombination(money - coins[checkIndex], coins, checkIndex);
+            int withoutFirstCoin = FindCombination(money, coins, checkIndex + 1);
+            return firstCoin + withoutFirstCoin;
+        }
+
+        void Display(int money, int[] coins)
+        {
+            Console.WriteLine("{0}-{1}", money, String.Join(",", coins.Select(x => x.ToString()).ToArray()));
+        }
+    }
+}
---
+++ CountingChangeCombinations/CountingChangeCombinationsTests/TestClass.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CountingChangeCombinations;
+
+namespace CountingChangeCombinationsTests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public static void SimpleCase()
+        {
+            Assert.AreEqual(3, Kata.CountCombinations(4, new[] { 1, 2 }));
+        }
+
+        [Test]
+        public static void AnotherSimpleCase()
+        {
+            Assert.AreEqual(4, Kata.CountCombinations(10, new[] { 5, 2, 3 }));
+        }
+
+        [Test]
+        public static void NoChange()
+        {
+            Assert.AreEqual(0, Kata.CountCombinations(11, new[] { 5, 7 }));
+        }
+
+        [Test]
+        public static void Anther1022Case()
+        {
+            Assert.AreEqual(1022, Kata.CountCombinations(300, new[] { 500, 5, 50, 100, 20, 200, 10 }));
+        }
+    }
+}
