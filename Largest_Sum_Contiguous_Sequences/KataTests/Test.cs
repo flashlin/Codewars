10e53c98b238d3441c9c47580254cb380d003340
flashlin <flash.lin@gmail.com>
2018-04-23 20:23:29 +0800

    add

---
+++ Largest_Sum_Contiguous_Sequences/KataTests/Test.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Largest_Sum_Contiguous_Sequences;
+
+namespace KataTests
+{
+	[TestFixture]
+	public class Test
+	{
+		[TestCase(new[] { -1, -2, -3 }, ExpectedResult = 0)]
+		[TestCase(new int[0], ExpectedResult = 0)]
+		[TestCase(new[] { 1, 2, 3, 4 }, ExpectedResult = 10)]
+		[TestCase(new[] { 31, -41, 59, 26, -53, 58, 97, -93, -23, 84 }, ExpectedResult = 187)]
+		public int TestFromDescription(int[] arr)
+		{
+			return Kata.LargestSum(arr);
+		}
+	}
+}
---
+++ Largest_Sum_Contiguous_Sequences/Largest_Sum_Contiguous_Sequences/Kata.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest_Sum_Contiguous_Sequences
+{
+	public static class Kata
+	{
+		public static int LargestSum(int[] arr)
+		{
+			int max = 0, tmp = 0;
+			for (int i = 0; i < arr.Length; i++)
+			{
+				tmp += arr[i];
+
+				if (max < tmp)
+				{
+					max = tmp;
+				}
+
+				if (tmp < 0)
+				{
+					tmp = 0;
+				}
+			}
+			return max;
+		}
+	}
+}
