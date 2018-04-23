e1ff45322f1966c55b6ef84a73c16632ae7b366e
flashlin <flash.lin@gmail.com>
2018-04-23 20:25:48 +0800

    refactor

--- Largest_Sum_Contiguous_Sequences/Largest_Sum_Contiguous_Sequences/Kata.cs
+++ Largest_Sum_Contiguous_Sequences/Largest_Sum_Contiguous_Sequences/Kata.cs
@@ -10,21 +10,13 @@ namespace Largest_Sum_Contiguous_Sequences
 	{
 		public static int LargestSum(int[] arr)
 		{
-			int max = 0, tmp = 0;
-			for (int i = 0; i < arr.Length; i++)
+			if (arr.Length <= 0)
 			{
-				tmp += arr[i];
-
-				if (max < tmp)
-				{
-					max = tmp;
-				}
-
-				if (tmp < 0)
-				{
-					tmp = 0;
-				}
+				return 0;
 			}
+
+			int max = 0;
+			max = arr.Max(n => max = Math.Max(0, max + n));
 			return max;
 		}
 	}
