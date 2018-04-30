cf3ea1bd1c3621c3bcaa3f027b4bfdbd82ecff36
flash <flash.lin@gmail.com>
2018-04-30 10:51:51 +0800

    add

---
+++ Pascal_Triangle/PascalTriangleTests/KataTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using Pascal_Triangle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PascalTriangleTests
+{
+    [TestFixture]
+    public class KataTest
+    {
+        [Test]
+        public static void Level1()
+        {
+            CollectionAssert.AreEqual(
+                new List<int> { 1 },
+                Kata.PascalsTriangle(1));
+        }
+
+        [Test]
+        public static void Level3()
+        {
+            CollectionAssert.AreEqual(
+                new List<int> { 1, 1, 1, 1, 2, 1 },
+                Kata.PascalsTriangle(3));
+        }
+
+        [Test]
+        public static void Level5()
+        {
+            CollectionAssert.AreEqual(
+                new List<int> {
+                    1,
+                    1, 1,
+                    1, 2, 1,
+                    1, 3, 3, 1,
+                    1, 4, 6, 4, 1 },
+                Kata.PascalsTriangle(5));
+        }
+    }
+}
---
+++ Pascal_Triangle/Pascal_Triangle/Kata.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pascal_Triangle
+{
+    public class Kata
+    {
+        public static List<int> PascalsTriangle(int n)
+        {
+            Console.WriteLine(n);
+            return PascalsTriangleDeep(n);
+        }
+
+        public static List<int> PascalsTriangleDeep(int n)
+        {
+            if (n == 1)
+            {
+                return PascalsTriangleLevel(1);
+            }
+
+            if (n == 2)
+            {
+                var level2 = PascalsTriangleLevel(2);
+                var level1 = PascalsTriangleLevel(1);
+                level1.AddRange(level2);
+                return level1;
+            }
+
+            var prev = PascalsTriangleDeep(n - 1);
+            prev.AddRange(PascalsTriangleLevel(n));
+            return prev;
+        }
+
+        public static List<int> PascalsTriangleLevel(int n)
+        {
+            if (n <= 0)
+            {
+                return new List<int>();
+            }
+
+            if (n == 1)
+            {
+                return new List<int> { 1 };
+            }
+
+            if (n == 2)
+            {
+                return new List<int> { 1, 1 };
+            }
+
+            var prevLevel = PascalsTriangleLevel(n - 1);
+            var level = new List<int>();
+            level.Add(1);
+            for (int i = 0; i < prevLevel.Count - 1; i++)
+            {
+                var val = prevLevel[i] + prevLevel[i + 1];
+                level.Add(val);
+            }
+            level.Add(1);
+            return level;
+        }
+    }
+}
