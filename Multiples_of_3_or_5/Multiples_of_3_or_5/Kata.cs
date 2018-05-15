fc6a8bd936d3dc1bb086d793a789710e4d63b27b
flash <flash.lin@gmail.com>
2018-05-15 10:37:30 +0800

    init

---
+++ Multiples_of_3_or_5/Multiples_of_3_or_5/Kata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiples_of_3_or_5
+{
+    public class Kata
+    {
+        public static int Solution(int value)
+        {
+            if (value <= 0)
+            {
+                return 0;
+            }
+            var data = Enumerable.Range(1, value - 1);
+            var query = from tb1 in data
+                where tb1 % 3 == 0 || tb1 % 5 == 0
+                select tb1;
+            return query.Sum();
+        }
+    }
+}
---
+++ Multiples_of_3_or_5/Multiples_of_3_or_5_Tests/KataTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Multiples_of_3_or_5;
+
+namespace Multiples_of_3_or_5_Tests
+{
+    [TestFixture]
+    public class KataTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(23, Kata.Solution(10));
+        }
+    }
+}
