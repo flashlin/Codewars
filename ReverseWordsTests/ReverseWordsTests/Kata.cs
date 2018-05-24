4eb2c225981fe27f54af42ac1beecca8f57b2beb
flashlin <flash.lin@gmail.com>
2018-05-24 21:01:40 +0800

    add

---
+++ ReverseWordsTests/ReverseWordsTests/Kata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWordsTests
+{
+	public static class Kata
+	{
+		public static string ReverseWords(string str)
+		{
+			string[] words = str.Split(' ');
+			var q1 = words.Select(x => Reverse(x));
+			return string.Join(" ", q1);
+		}
+
+		public static string Reverse(string s)
+		{
+			char[] charArray = s.ToCharArray();
+			Array.Reverse(charArray);
+			return new string(charArray);
+		}
+	}
+}
---
+++ ReverseWordsTests/ReverseWordsTests/KataTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWordsTests
+{
+	[TestFixture]
+	public class KataTest
+	{
+		[Test]
+		public void Example()
+		{
+			Assert.AreEqual("sihT si na !elpmaxe", Kata.ReverseWords("This is an example!"));
+		}
+	}
+}
